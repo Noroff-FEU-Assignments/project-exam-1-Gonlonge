a5463b59f31d387bec1b6328b90c00abadd0eb7b
Gonzalo Longe <gonlonge@icloud.com>
2022-05-03 15:23:14 +0200

    update

---
+++ JS/contact.js
@@ -0,0 +1,71 @@
+const form = document.querySelector("#contactForm");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (checkLength(firstName.value, 5) === true) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+  }
+
+  if (checkLength(lastName.value, 5) === true) {
+    lastNameError.style.display = "none";
+  } else {
+    lastNameError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+  if (checkLength(subject.value, 15) === true) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+  if (checkLength(lastName.value, 25) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+
+  console.log("hello");
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
+
+// ### Contact page
+
+// Create a contact us page, there should be 4 textboxes on this page.
+// -	Name (Should be more than 5 characters long)
+// -	Email address (Must be a valid email address)
+// -	Subject (Should be more than 15 characters long)
+// -	Message content (Should be more than 25 characters long)
+
+// Please use JavaScript for validation, show error messages if the values in the textboxes do not meet the requirements.
--- JS/script.js
+++ JS/script.js
@@ -1,58 +1,72 @@
 const url = "https://blogofnorway.site/blogofnorway/wp-json/wp/v2/posts";
+const blogResults = document.querySelector(".blog-results");
 
 async function getProducts(url) {
-  try {
-    const response = await fetch(url);
-    const getResults = await response.json();
-    console.log(getResults);
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(url);
+  const getResults = await response.json();
+  console.log(getResults);
+
+  getResults.forEach((result) => {
+    blogResults.innerHTML += `<div>${result.content.rendered}</div>`;
+  });
 }
 
-getProducts(baseUrl);
+getProducts(url);
 
-/* 
-getResults.forEach((game) => {
-    productContainer.innerHTML += `<a href="buy.html?id=${game.id}">
-    <div class="products">
-    <div class="product-img">
-    <div class="color-js">
-    <img src="${game.images[0].src}">
-    <h3>${game.name} ${game.prices.price} ${game.prices.currency_code}</h3>
-    </div>
-    </div>
-    </div>
-    </a>`;
-  });
+// }
 
-  */
+// const loading = document.querySelector(".loader");
+// loading.classList.remove("loading-indicator");
+// } catch (error) {
+// console.log(error);
+// container.innerHTML = "Error when its calling to the API";
+// }
+// }
+// fetchMovies();
+// */
 
 /*
-  const productUrl = "https://drglutes.no/gamehub/wp-json/wc/store/products/";
-const productContainer = document.querySelector(".products-list");
+async function fetchMovies() {
+  const container = document.querySelector(".results");
 
+  try {
+    const response = await fetch("https://the-one-api.dev/v2/movie", {
+      method: "get",
+      headers: new Headers({
+        Authorization: "Bearer cx4oxkxUmuKp-12KrpOF",
+      }),
+    });
 
-  async function getProduct(productId) {
-    try {
-      const response = await fetch(productUrl + productId);
-      const game = await response.json();
-  
-      const cartPrice = document.querySelector(".cart-price");
-      const description = document.querySelector(".game-description");
-      const img = document.querySelector(".game-image");
-  
-      console.log(game);
-      console.log(game.images[0].src);
-      console.log(game.name);
-      console.log(game.prices.price);
-      console.log(game.prices.currency_code);
-      console.log(game.description);
-  
-      cartPrice.innerHTML = `${game.name} ${game.prices.currency_code} ${game.prices.price} `;
-      description.innerHTML = `${game.description}`;
-  
-      img.innerHTML = `<img src="${game.images[0].src}"/>`;
-    } catch (error) {}
+    if (response.status !== 200) {
+      container.innerHTML = "API error";
+      return;
+    }
+
+    const results = await response.json();
+    const movies = results.docs;
+
+    if (!movies) {
+      container.innerHTML = "Unexpected response from API";
+      return;
+    }
+
+    for (let i = 0; i < movies.length; i++) {
+      const movie = movies[i];
+
+      container.innerHTML += `<div class="space">
+                                <a href="details.html?id=${movie._id}">
+                                  <div>Name: ${movie.name}</div>
+                                  <div>Length: ${movie.runtimeInMinutes}minutes</div>
+                                </a>
+                              </div>`;
+    }
+
+    const loading = document.querySelector(".loader");
+    loading.classList.remove("loading-indicator");
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = "Error when its calling to the API";
   }
-  */
+}
+fetchMovies();
+*/
